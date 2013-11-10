82e98496ffa5f63d1ae581769d9fccef1bb8ce79
Nathan Zadoks <nathan@nathan7.eu>
2013-11-11 00:46:17 +0100

    add usage

--- term.js
+++ term.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+var port = process.argv[2]
+  , baud = process.argv[3]
+
+if (process.argv.length < (2 + 1) || process.argv.length > (2 + 2) || (baud && !baud.match(/^\d+$/))) {
+  console.log('usage: term <port> [baudrate]')
+  process.exit(1)
+}
+
 var Serial = require('./')
+  , fs = require('fs')
+
 process.stdin.setRawMode(true)
 process.stdin.pipe(new Serial(process.argv[2], process.argv[3] | 0)).pipe(process.stdout)
